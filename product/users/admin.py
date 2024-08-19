3a8b739a6fbbede5ec18ef5db653949f901aaee1
Valeriya Dudchenko <vvd.2209@bk.ru>
2024-08-19 23:40:19 +0300

    Реализация проекта

--- product/users/admin.py
+++ product/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import CustomUser, Balance, Subscription
+
+
+admin.site.register(CustomUser)
+
+
+@admin.register(Balance)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('student', 'bonus_points',)
+
+
+@admin.register(Subscription)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('course', 'student',)

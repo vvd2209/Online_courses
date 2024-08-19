5a960481cb5cc96ee74b0aea45192ef617ca77e2
Valeriya Dudchenko <vvd.2209@bk.ru>
2024-08-19 23:40:09 +0300

    Реализация проекта

--- product/courses/admin.py
+++ product/courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Course, Lesson, Group
+
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Group)
